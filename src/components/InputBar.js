1534ebd98d04e923428ab05d7994e62dc260ff56
Abby <smitha9294@gmail.com>
2021-06-28 04:12:05 -0500

    fixed quantity default value

--- src/components/InputBar.js
+++ src/components/InputBar.js
@@ -2,12 +2,12 @@ import React, {useState} from 'react'
 
 const InputBar = ({handleSubmit}) => {
 	const [item, setItem] = useState('')
-	const [quantity, setQuantity] = useState('')
+	const [quantity, setQuantity] = useState(1)
 
 	const handleOnSubmit = () => {
 		handleSubmit(item, quantity)
 		setItem('')
-		setQuantity('')
+		setQuantity(1)
 	}
 
 	return (
@@ -19,7 +19,7 @@ const InputBar = ({handleSubmit}) => {
 				</div>
 				<div className="inputFieldDiv">
 					<input type="text" className="item" name="item" id="item" placeholder="Enter name of item" onChange={(e) => setItem(e.target.value)} value={item} required />
-					<input type="number" className="quantity" name="quantity" id="quantity" onChange={(e) => setQuantity(parseInt(e.target.value))} value={quantity} />
+					<input type="number" className="quantity" name="quantity" id="quantity" onChange={(e) => setQuantity(parseInt(e.target.value))} value={quantity} required />
 					<button type="submit">Add Item</button>
 				</div>
 			</form>
